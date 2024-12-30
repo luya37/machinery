1bc1d87c80563c25e26ee97c79e1148bbb97f693
Ramond <ramond@adion.ai>
2024-12-30 14:30:03 +0800

    no need set

--- v2/brokers/redis/goredis.go
+++ v2/brokers/redis/goredis.go
@@ -55,7 +55,6 @@ func NewGR(cnf *config.Config, addrs []string, db int) iface.Broker {
 	}
 	if cnf.Redis != nil {
 		ropt.MasterName = cnf.Redis.MasterName
-		ropt.SentinelPassword = password
 	}
 
 	if cnf.Redis != nil && cnf.Redis.SentinelPassword != "" {
